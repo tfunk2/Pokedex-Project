75ff4c6a0726ffcdf63c3fbf2f95768ee4acf313
tfunk2 <tyler.j.funk2@gmail.com>
2020-03-03 17:38:18 -0700

    test

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,5 +4,7 @@ class Cli
     puts "Which of the 151 Pokemon would you like to learn more about?"
     user_response = gets.chomp
     end
-    
+
+    def hey
+    end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
     has_many :favorite_pokemon
-    has_many :pokemon, through: :favorite_pokemon
+    has_many :pokemon, through: :favorite_pokemon 
 end
