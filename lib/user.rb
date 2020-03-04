b5b2eaf51fbc506dc63df526c37828c89a48e675
kadrianne <kristine.a.du@gmail.com>
2020-03-03 17:11:49 -0700

    Add favorite_pokemon_list method.

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
     has_many :favorite_pokemon
     has_many :pokemon, through: :favorite_pokemon
+
+    def favorite_pokemon_list
+        pokemon.map do |favorite|
+            favorite.name
+        end
+    end
 end
