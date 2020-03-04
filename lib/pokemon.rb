e66ab5da11eb5b7ae506656ce3aa41e441eb60d4
kadrianne <kristine.a.du@gmail.com>
2020-03-04 13:42:49 -0700

    Update display_pokemon_info method.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,13 +4,13 @@ class Pokemon < ActiveRecord::Base
     belongs_to :type
 
     def self.select_pokemon_by_name(pokemon_name)
-        found_pokemon = all.find_by(name: pokemon_name)
-        if !found_pokemon
+        @found_pokemon = all.find_by(name: pokemon_name)
+        if !@found_pokemon
             puts "Could not find that Pokemon. Please try your search again."
             puts "\n"
             main_menu(user)
         else
-            display_pokemon_info(found_pokemon)
+            @found_pokemon.display_pokemon_info
         end
     end
 
@@ -24,7 +24,21 @@ class Pokemon < ActiveRecord::Base
             display_pokemon_info(found_pokemon_by_id)
         end
     end
-     
+    
+    def display_pokemon_info
+        #Figure out how to render this image in terminal
+        #Front sprite
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/#{pokemon_id}.png"
+        #Back sprite
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/#{pokemon_id}.png"
+        puts "Name: #{name.capitalize}"
+        puts "Pokemon ID: #{pokemon_id}"
+        puts "Height: #{height}"
+        puts "Weight: #{weight}"
+        puts "Type: #{type_1.capitalize}"
+        #figure out what menu follows this
+    end
+
     def list_pokemon_by_type(pokemon_type)
     
     end
