1ba834f6172d08ca5181908ce7861626c203472f
kadrianne <kristine.a.du@gmail.com>
2020-03-03 21:47:44 -0700

    Update menus file.

--- lib/menus.rb
+++ lib/menus.rb
@@ -30,26 +30,4 @@ def type_menu
     }
     poke_type_response = prompt.select("Select a Pokemon Type:", types)
 end
-
-def select_pokemon_by_name(pokemon_name)
-    # if !Pokemon.all.find_by(name: pokemon_name)
-    #     puts "Could not find that Pokemon. Please try your search again."
-    #     main_menu
-    # else
-    #     display_pokemon_info(pokemon_name)
-    # end
-
-end
-
-def select_pokemon_by_id(pokemon_id)
-
-end
-
-def select_pokemon_by_type()
-
-end
-
-def display_pokemon_info(pokemon_name)
-
-end
 # binding.pry
